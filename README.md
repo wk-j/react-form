fd5c87ed60e77e8fbe268aec469cb3dae3f8d7eb
wk-j <somnuk.wk@gmail.com>
2019-05-17 15:15:24 +0700

    Form

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Form
+## React Form
 
 ```bash
 npm install
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,70 @@
 import React from "react"
 import { render } from "react-dom"
 
-export class App extends React.Component<{}, {}> {
+type Form = {
+    fieldA: string
+    fieldB: string
+    fieldC: string
+}
+
+type Forms = keyof (Form)
+
+type Props = {}
+
+type State = {
+    data: Form
+}
+
+export class App extends React.Component<Props, State> {
     constructor(props) {
         super(props)
+
+        this.state = {
+            data: {
+                fieldA: "field-a",
+                fieldB: "field-a",
+                fieldC: "field-a"
+            }
+        }
+    }
+
+    onChange = (key: Forms) => (e) => {
+        var value = e.target.value;
+        let data = this.state.data;
+        data[key] = value
+
+        this.setState({
+            data: data
+        })
+    }
+
+    show = (e) => {
+        console.log(this.state.data);
     }
 
     render() {
+        var data = this.state.data;
         return (
-            <h1>Hello, world!</h1>
+            <div>
+                <div>
+                    <label>Field A</label>
+                    <input type="text" value={data.fieldA} onChange={this.onChange("fieldA")} />
+                </div>
+                <div>
+                    <label>Field B</label>
+                    <input type="text" value={data.fieldB} onChange={this.onChange("fieldB")} />
+                </div>
+                <div>
+                    <label>Field C</label>
+                    <input type="text" value={data.fieldC} onChange={this.onChange("fieldC")} />
+                </div>
+
+                <div>
+                    <button onClick={this.show}>Show</button>
+                </div>
+
+                <h1>Hello, world!</h1>
+            </div>
         )
     }
 }
